beb019a7977bdd054a163ba95b6ed3341d3f4175
divyashah98 <ddsdivya.98@gmail.com>
2017-06-05 21:40:09 +0530

    Update P1CurrentThreadDemo.java

--- P1CurrentThreadDemo.java
+++ P1CurrentThreadDemo.java
@@ -2,19 +2,28 @@
 
 class P1CurrentThreadDemo
 {
-public static void main(String args[]) {
+public static void main(String args[]) 
+{
 Thread t = Thread.currentThread();
 System.out.println("Current thread: " + t);
-// change the name of the thread
+
 t.setName("My Thread");
 System.out.println("After name change: " + t);
-try {
-for(int n = 5; n > 0; n--) {
+
+try 
+{
+for(int n = 5; n > 0; n--) 
+{
 System.out.println(n);
 Thread.sleep(1000);
 }
-} catch (InterruptedException e) {
+} 
+
+catch (InterruptedException e) 
+{
 System.out.println("Main thread interrupted");
 }
+
 }
+
 }
