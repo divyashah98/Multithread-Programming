ce05e7aa53f59066b51b0d6fae882451f8b7089b
divyashah98 <ddsdivya.98@gmail.com>
2017-06-05 21:37:24 +0530

    Add files via upload

---
+++ P1CurrentThreadDemo.java
@@ -0,0 +1,20 @@
+/*  class CurrentThreadDemo */
+
+class P1CurrentThreadDemo
+{
+public static void main(String args[]) {
+Thread t = Thread.currentThread();
+System.out.println("Current thread: " + t);
+// change the name of the thread
+t.setName("My Thread");
+System.out.println("After name change: " + t);
+try {
+for(int n = 5; n > 0; n--) {
+System.out.println(n);
+Thread.sleep(1000);
+}
+} catch (InterruptedException e) {
+System.out.println("Main thread interrupted");
+}
+}
+}
